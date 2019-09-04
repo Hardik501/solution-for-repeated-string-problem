846ba6734529c3917845a108d5c8da8cf2280aa9
Hardik501 <32429687+Hardik501@users.noreply.github.com>
2019-09-04 19:11:50 +0530

    Create repeated_string_solved.py

---
+++ repeated_string_solved.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import string
+
+# Complete the repeatedString function below.
+def repeatedString(s, n):
+    a=0
+    m = s.count('a')
+    if m==0:
+        return 0
+    elif m==1 and len(s)==1:
+        return n
+    else:
+        r=int(n/len(s))
+        rr=int(n%len(s))
+        return(m*r+s[:rr].count('a'))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input())
+
+    result =int(repeatedString(s, n))
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
